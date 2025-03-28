21496168361985ca0f52fad699893ee9cab526ba
Atul Reny <117465987+AtulRy@users.noreply.github.com>
2025-03-29 00:22:29 +0530

    IST set

--- controllers/generateOtp.js
+++ controllers/generateOtp.js
@@ -1,7 +1,12 @@
 const crypto=require('crypto')
 
 function generateDailyOtp(){
-    const date = new Date().toISOString().slice(0, 10); // "YYYY-MM-DD"
+    const istOffset = 19800 * 1000; // 5 hours 30 minutes in milliseconds
+    const istTime = new Date(now.getTime() + istOffset);
+
+    // Format to "YYYY-MM-DD"
+    const date = istTime.toISOString().slice(0, 10);
+   
     const hash = crypto.createHash('sha256').update(date).digest('hex');
     // console.log(hash);
      const numbersOnly = hash.replace(/\D/g, ''); // Remove non-digits
